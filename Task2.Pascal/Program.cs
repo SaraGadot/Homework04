fe63ba657fe7911ba2b7d033410df9019b22bebd
Sara Gadot <->
2021-05-14 00:12:36 +0300

    расчет треугольника паскаля

---
+++ Task2.Pascal/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task2.Pascal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = 9;
+            var pascal = new int[N, N];
+
+
+            for (var y = 0; y < N; y++)
+            {
+                pascal[0, y] = 1;
+            }
+
+            for (var x = 0; x < N; x++)
+            {
+                pascal[x, 0] = 1;
+            }
+            for (var row = 1; row < N; row++)
+            {
+                for (var column = 1; column < row; column++)
+                {
+                    var x = row - column;
+                    var y = column;
+                    pascal[x, y] = pascal[x, y - 1] + pascal[x - 1, y]; 
+                }
+            }
+            for (var row = 0; row < N; row++)
+            {
+                for (var column = 0; column < row + 1; column++)
+                {
+                    var x = row - column;
+                    var y = column;
+                    // Console.Write($"{x},{y} ");
+                    Console.Write($"{pascal[x, y]} ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            // * Задание 2
+            // Заказчику требуется приложение строящее первых N строк треугольника паскаля. N < 25
+            // 
+            // При N = 9. Треугольник выглядит следующим образом:
+            //                                 1
+            //                             1       1
+            //                         1       2       1
+            //                     1       3       3       1
+            //                 1       4       6       4       1
+            //             1       5      10      10       5       1
+            //         1       6      15      20      15       6       1
+            //     1       7      21      35      35       21      7       1
+            //                                                              
+            //                                                              
+            // Простое решение:                                                             
+            // 1
+            // 1       1
+            // 1       2       1
+            // 1       3       3       1
+            // 1       4       6       4       1
+            // 1       5      10      10       5       1
+            // 1       6      15      20      15       6       1
+            // 1       7      21      35      35       21      7       1
+            // 
+            // Справка: https://ru.wikipedia.org/wiki/Треугольник_Паскаля
+
+        }
+    }
+}
